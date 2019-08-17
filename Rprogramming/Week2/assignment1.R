d784b1d4a5570e728bb7a0ca9e832735d2911223
Robert Muwanga <robert.muwanga@outlook.com>
2019-08-17 19:48:37 +0300

    Developed corr function.

--- Rprogramming/Week2/assignment1.R
+++ Rprogramming/Week2/assignment1.R
@@ -1,5 +1,13 @@
 library(tidyverse)
-unzip('rprog_data_specdata.zip')
+
+url <- 'https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip'
+fn <- 'rprog_data_specdata.zip'
+
+if(!file.exists(filename)) 
+   download.file(url = url, destfile = 'rprog_data_specdata.zip')
+
+if(!dir.exists('specdata'))
+  unzip('rprog_data_specdata.zip')
 
 getFiles <- function(directory, id = 1:332) {
   data <- lapply(
@@ -7,6 +15,11 @@ getFiles <- function(directory, id = 1:332) {
       read_csv(file.path('specdata', x)) %>% 
         mutate('filename' = x) }) %>% 
     bind_rows() 
+  
+# Consider removing filename as:
+# > all(data$filename %>% str_remove(pattern = '.csv') %>% as.integer() == data$ID) //ALL IS TRUE
+# You can consider making mention of this as a quick verification in your Rmarkdown file.
+
 }
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
@@ -26,3 +39,15 @@ complete <- function(directory, id = 1:332) {
                   'nobs' = complete.cases(.) %>% sum)
     ) %>% bind_rows
 }
+
+corr <- function(directory, threshold = 0) {
+  complete_cases <- complete(directory) %>% 
+    filter(nobs >= threshold)
+  
+  data <- getFiles(directory, complete_cases$id)
+  
+  data[complete.cases(data), ] %>%
+    group_by(filename) %>%
+    summarize('corr' = cor(x = sulfate, y = nitrate))
+}
+
